e071daf20de9c0ef0b407a440bc7bc0107b4524b
Amy Roberts <uncamy@gmail.com>
2013-10-17 11:58:42 -0400

    fixed bug and created training deck

--- readCard.py
+++ readCard.py
@@ -2,10 +2,14 @@
 import cv2
 import numpy as np
 
-numcards = 4
 
-#read in the file
-im =cv2.imread('card_example.png')
+numcards = 56
+
+#im =cv2.imread('twoCards.png')
+#im =cv2.imread('glass_example.png')
+#im =cv2.imread('card_example.png')
+im =cv2.imread('training_cards.png')
+#im =cv2.imread('glass_training_deck.png')
 
 #preporcess to remove artifacts
 gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
@@ -15,24 +19,25 @@ flag, thresh = cv2.threshold(blur, 120, 255, cv2.THRESH_BINARY)
 #find the contours
 contours, hierarchy =cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contours = sorted(contours, key= cv2.contourArea, reverse =True) [:numcards]
+contour_img = cv2.drawContours(im, contours,  -1, (0,255,0),3)
 
 #display image
 def display_image(image):
     """displays image in the python REPL"""
     cv2.namedWindow('Display Window')
-    cv2.imshow('Display Window', image)
+    cv2.imshow('Display Window', im)
 
-for i in range(numcards):
-    card = contours[i]
-    peri = cv2.arcLength(card, True)
-    approx = np.array((cv2.approxPolyDP(card, 0.02*peri, True)), np.float32)
-    rect = cv2.minAreaRect(contours[2])
-    r = cv2.cv.BoxPoints(rect)
-    
-    h = np.array([ [0,0],[449,0],[449,449],[0,449] ],np.float32)
-
-    transform = cv2.getPerspectiveTransform(approx,h)
-    warp =cv2.warpPerspective(im, transform, (450, 450))
+def process_image(numcards):
+    for i in range(numcards):
+        card = contours[i]
+        peri = cv2.arcLength(card, True)
+        approx = np.array((cv2.approxPolyDP(card, 0.02*peri, True)), np.float32)
+        rect = cv2.minAreaRect(contours[i])
+        r = cv2.cv.BoxPoints(rect)
+        h = np.array([ [0,0],[449,0],[449,449],[0,449] ],np.float32)
+        transform = cv2.getPerspectiveTransform(approx,h)
+        warp =cv2.warpPerspective(im, transform, (450, 450))
 
 display_image(im)
+
 
