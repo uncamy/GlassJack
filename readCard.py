f82e75128d63cb105c25bb9fa5e3fbc53602d4a3
Amy Roberts <uncamy@gmail.com>
2013-11-06 12:43:17 -0500

    added blackjack score from tsv + corrected typos

--- readCard.py
+++ readCard.py
@@ -1,15 +1,16 @@
 # This file uses image recognition to identify the cards that are currently on the table
 import cv2
+import sys
 import numpy as np
 sys.path.insert(0, "/usr/local/lib/python2.7/site-packages/")
 
 #numcards = 56
 
-#img =cv2.imread('twoCards.png')
-#img =cv2.imread('glass_example.png')
-#img =cv2.imread('card_example.png')
-#img =cv2.imread('training_cards.png')
-#img =cv2.imread('glass_training_deck.png')
+#im =cv2.imread('twoCards.png')
+#im =cv2.imread('glass_example.png')
+#im =cv2.imread('card_example.png')
+#im =cv2.imread('training_cards.png')
+#im =cv2.imread('glass_training_deck.png')
 
 #Image matching
 def rectify(h):
@@ -25,34 +26,35 @@ def rectify(h):
     hnew[3] = h[np.argmax(diff)]
     return hnew
 
-def preprocess(img):
+def preprocess(im):
     """preporcess to remove artifacts"""
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (1,1), 1000)
     flag, thresh = cv2.threshold(blur, 120, 255, cv2.THRESH_BINARY)
     return thresh
 
-def imgdiff(img1, img2):
-    img1 = cv2.GaussianBlur(img1, (5,5),5)
-    img2 = cv2.GaussianBlur(img2, (5,5),5)
-    diff = cv2.absdiff(img1, img2)
+def imgdiff(im1, im2):
+    im1 = cv2.GaussianBlur(im1, (5,5),5)
+    im2 = cv2.GaussianBlur(im2, (5,5),5)
+    diff = cv2.absdiff(im1, im2)
     diff = cv2.GaussianBlur(diff, (5,5),5)
     flag, diff = cv2.threshold(diff, 200, 255, cv2.THRESH_BINARY)
     return np.sum(diff)
 
-def find_closet_card(training, img):
-    features = preporcess(img)
+def find_closest_card(training, im):
+    features = preprocess(im)
     return sorted(training.values(), key=lambda x:imgdiff(x[1], features))[0][0]
 
 #Getting cards
 def getCards(im, numcards=4):
     """remove artifacts"""
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (1,1), 1000)
     flag, thresh = cv2.threshold(blur, 120, 255, cv2.THRESH_BINARY)
 
     #find the contours
-    contours, hierarchy =cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy =cv2.findContours(thresh, cv2.RETR_TREE,\
+                            cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key= cv2.contourArea, reverse =True) [:numcards]
 
     for card in contours:
@@ -60,37 +62,40 @@ def getCards(im, numcards=4):
         approx = rectify(cv2.approxPolyDP(card, 0.02*peri, True))
         h = np.array([ [0,0],[449,0],[449,449],[0,449] ],np.float32)
         transform = cv2.getPerspectiveTransform(approx,h)
-        warp =cv2.warpPerspective(img, transform, (450, 450))
+        warp =cv2.warpPerspective(im, transform, (450, 450))
 
         yield warp
 
-    #contour_img = cv2.drawContours(img, contours,  -1, (0,255,0),3)
-def get_training(training_labels_filename, training_image_filenem, num_trainingcards, avid_cards = None):
+    #contour_im = cv2.drawContours(im, contours,  -1, (0,255,0),3)
+def get_training(training_labels_filename, training_image_filename,\
+                 num_training_cards, avoid_cards = None):
     training = {}
 
     labels = {}
     for line in file(training_labels_filename):
-        key, num, suit = line.strip().split()
-        labels[int(key)] = (num, suit)
+        key, num, suit, score = line.strip().split()
+        labels[int(key)] = (num, suit, score)
 
     print "Training"
 
     im = cv2.imread(training_image_filename)
     for i, c in enumerate(getCards(im, num_training_cards)):
-        if avoid_cards is None or (labels[i][0] not in avoid_cards[0] and labels[i][1] not in avoid_cards[1]):
+        if avoid_cards is None or (labels[i][0] not in avoid_cards[0] and\
+                                   labels[i][1] not in avoid_cards[1]):
             training[i] = (labels[i], preprocess(c))
     print "Done Training"
     return training
 
 if __name__ == '__main__':
-    if len(sys.argv) == 6:
+    if (len(sys.argv) == 6):
         filename = sys.argv[1]
         num_cards = int(sys.argv[2])
         training_image_filename = sys.argv[3]
         training_labels_filename = sys.argv[4]
-        num_training_Cards = int(sys.argv[5])
+        num_training_cards = int(sys.argv[5])
 
-        training = get_training(training_labels_filename, training_image_filename, num_training_cards)
+        training = get_training(training_labels_filename,\
+                                training_image_filename, num_training_cards)
         im = cv2.imread(filename)
         width = im.shape[0]
         height = im.shape[1]
@@ -111,4 +116,4 @@ def display_image(image):
 
 
 
-#display_image(img)
+#display_image(im)
