42c90be3a7b20751d7a2ae7dab7030bbd28d8b4c
uncamy@gmail.com <uncamy@gmail.com>
2013-12-02 14:21:33 -0500

    added score fxn to calculate players hand

--- pickMove.py
+++ pickMove.py
@@ -5,30 +5,37 @@ import readCard
 #returned from cardReader --> card 1 and card 2
 test_ace =[('A', 'S', 'A'), ('J', 'H', '10'), ('3', 'H', '3')]
 
-#consider adding a turn count this is addequate for all game states
+#consider adding a turn count. this is not adequate for all game states
 def whos_card(cards):
     if len(cards) <5:
         player_hand = cards[:2]
-        dealer_hand = cards[3]
+        dealer_hand = cards[2:]
     else:
         player_hand = cards[:3]
         dealer_hand = cards[2:]
+    print 'Player\'s Hand is: %s' % player_hand
+    print 'Dealer\'s Hand is: %s' % dealer_hand
+    return player_hand, dealer_hand
 
-def score(card1, card2):
-
-
-if (card1 or card2 == 'A'):
-        hand = (card1 + card2)
-        my_hand = (','.join(hand))
-    else:
-        my_hand = int(card1) + int(card2)
-
-    return my_hand
-
+def score(hand):
+    if len(hand) > 1:
+        if hand[0][0] == hand[0][1]:
+            print 'We have two matching cards'
+            return hand
+        elif hand[0][0] == 'A' or hand[0][1] == 'A':
+            print 'We have an Ace!'
+            return hand
+        else:
+            sum_hand = str(int(hand[0][0]) + int(hand[0][1]))
+            print 'We have a score of %s' %sum_hand
+            return sum_hand
 
 def main():
   cards = readCard.main()
   card_value = [x[2] for x in cards]
+  card_owner = whos_card(card_value)
+  score(card_owner)
+
   check4ace= map(score, card_value)
 
 '''
