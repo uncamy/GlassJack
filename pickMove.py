a59a561df9034c816527b7b676a4aeee40853447
Allison Kaptur <allison.kaptur@gmail.com>
2013-12-03 14:59:25 -0500

    refactor scoring

--- pickMove.py
+++ pickMove.py
@@ -6,35 +6,24 @@ from theBook import card_book
 test_ace =[('A', 'S', 'A'), ('J', 'H', '10'), ('3', 'H', '3')]
 test_pair =[('7', 'S', '7'), ('7', 'H', '7'), ('3', 'H', '3')]
 
-#consider adding a turn count. this is not adequate for all game states
-def whos_card(cards):
+#refactor such that formatting doesn't need to be repeated for each case
+def score(cards):
     assert len(cards) == 3
     dealer_value = cards.pop()
     player_hand = cards
-    print "Player's Hand is: %s" % player_hand
-    print "Dealer's Hand is: %s" % dealer_hand
-    return player_hand, dealer_value
 
-#refactor such that formatting doesn't need to be repeated for each case
-def score(hand):
-    if len(hand) > 1:
-        if hand[0][0] == hand[0][1]:
-            print 'We have two matching cards'
-            player_hand = ('%s,%s') %(hand[0][0], hand[0][1])
-        elif hand[0][0] == 'A':
-            print 'We have an Ace!'
-            player_hand = ('A,%s') %(hand[0][1])
-        elif hand[0][1] == 'A':
-            print 'We have an Ace!'
-            player_hand = ('A,%s') %(hand[0][0])
-        else:
-            player_hand = str(int(hand[0][0]) + int(hand[0][1]))
+    if 'A' in player_hand:
+        print 'We have an Ace!'
+        player_hand.sort(reverse=True) # move Ace to first spot
+    elif player_hand[0] != player_hand[1]: # don't add doubles
+        player_hand = str(sum(int(card) for card in player_hand))
+
+    player_hand = ",".join(player_hand)
     print 'Your hand is %s' %player_hand
 
-    dealer_hand = ('%s') %hand[1][0]
-    print 'The dealer\'s hand is %s' %dealer_hand
+    print "The dealer's hand is %s" %dealer_value
 
-    score_round = (player_hand, dealer_hand)
+    score_round = (player_hand, dealer_value)
     print score_round
     return score_round
 
@@ -54,8 +43,7 @@ def pick_move(score):
 def game_main():
   cards = readCard.main()
   card_value = [x[2] for x in cards]
-  card_owner = whos_card(card_value)
-  scored = score(card_owner)
+  scored = score(card_value)
   move= pick_move(scored)
   print move
   return move
